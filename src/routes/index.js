f181c42911d03ede1e9a1c60d40b74a8096dcda3
Fernando Tello <fernando_tellol@hotmail.com>
2022-01-15 13:48:16 -0500

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+module.exports = app => {
+  const routerMarca = require('./routesMarca');
+
+  // const routerUsuario = require('./routesUsuario');
+
+  app.use('/marca', routerMarca);
+  // app.use('/usuario', routerUsuario);
+};
