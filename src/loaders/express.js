4a5e97d7423a55c5d92e4c4b1dd6eddc3fd2acf6
ricardo <rbalbis2001@gmail.com>
2022-01-25 22:16:15 -0500

    se añade signo de coma en la propieda APIS

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -34,7 +34,7 @@ const swaggerOptions = {
     `${path.join(__dirname,"../routes/routesMarca.js")}`,
     `${path.join(__dirname,"../routes/routesModelo.js")}`,
     `${path.join(__dirname,"../routes/routesTipoUsuario.js")}`,
-    `${path.join(__dirname,"../routes/routesUsuario.js")}`
+    `${path.join(__dirname,"../routes/routesUsuario.js")}`,
     `${path.join(__dirname,"../routes/routesEquipo.js")}`
 
   ]
