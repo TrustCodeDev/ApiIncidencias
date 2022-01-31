c5c19449194db185fc0c05204542c081b7df3287
Fernando Tello <fernando_tellol@hotmail.com>
2022-01-31 09:37:44 -0500

    actualizando archivo env

--- src/routes/routesArea.js
+++ src/routes/routesArea.js
@@ -1,6 +1,53 @@
 const router = require('express').Router();
 const area = require('../apiService/area/controller');
 
+/**
+ * @swagger
+ *
+ * components:
+ *  schemas:
+ *    area:
+ *      type: object
+ *      properties:
+ *        id_equipo:
+ *          type: integer
+ *          description: id autogenerado del equipo
+ *        descripcion:
+ *          type: string
+ *          description: descripcion del equipo
+ *        eliminado:
+ *          type: integer
+ *          description: indica 1 si esta inactivo 0 si esta activo
+ *        f_create:
+ *          type: date
+ *          description: fecha automatica de creacion
+ *        f_update:
+ *          type: date
+ *          description: fecha automatica de actulizacion
+ *        u_create:
+ *          type: string
+ *          description: usuario de creacion
+ *        u_update:
+ *          type: string
+ *          description: usuario de actualizacion
+ *        id_modelo:
+ *          type: integer
+ *          description: id del modelo
+ *      required:
+ *        - descripcion
+ *        - eliminado
+ *        - u_create
+ *        - modelo_id_modelo
+ *      example:
+ *        descripcion: Intel I-7-10
+ *        eliminado: 1
+ *        f_create: 2022-01-18 22:43:09
+ *        f_update: 2022-01-18 22:43:09
+ *        u_create: rbueno
+ *        u_update: admin
+ *        id_modelo: 1
+ */
+
 // Create a new Area
 router.post('/', area.create);
 
